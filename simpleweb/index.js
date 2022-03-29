a63f9421317fee9c16bcceb23047db28d234b6b6
Prajwal <prajwalhalwai@pm.me>
2022-03-29 14:54:56 +0200

    initial commit

---
+++ simpleweb/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi there');
+});
+
+app.listen(8080, () => {
+    console.log('Simple web running on 8080');
+});
